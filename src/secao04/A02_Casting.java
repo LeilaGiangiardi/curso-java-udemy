6ed76ed039606616f7e317e9c275d833964224a4
Leila Giangiardi <leila.giangiardi@ufrpe.br>
2026-02-23 16:54:24 -0300

    feat: exercícios e notas sobre casting de variáveis da sessão 04

---
+++ src/secao04/A02_Casting.java
@@ -0,0 +1,13 @@
+package src.secao04;
+
+public class A02_Casting {
+    public static void main(String[] args) {
+        //casting 
+        int a, b;
+        double resultado;
+        a = 5;
+        b = 2;
+        resultado = (double) a / b;
+        System.out.println(resultado);
+    }
+}
