962510ae6defc39bcf5df7e23fa51c2c54ba0cb5
Leila Giangiardi <leila.giangiardi@ufrpe.br>
2026-02-24 17:31:30 -0300

    🎉 feat(seção 5): inicia seção 05 e adiciona anotações sobre expressões comparativas

---
+++ src/sc05_estrutura_condicional/A01_expressoes_comparativas.java
@@ -0,0 +1,25 @@
+package src.sc05_estrutura_condicional;
+
+public class A01_expressoes_comparativas {
+
+	public static void main(String[] args) {
+		
+		// Suponha x igual a 5
+		int x = 5;
+		
+		System.out.println("--- Exemplos de Expressões Comparativas ---");
+		
+		// x > 0 -> Resultado: V (true)
+		System.out.println("x > 0: " + (x > 0)); 
+		
+		// x == 3 -> Resultado: F (false)
+		System.out.println("x == 3: " + (x == 3)); 
+		
+		// 10 <= 30 -> Resultado: V (true)
+		System.out.println("10 <= 30: " + (10 <= 30)); 
+		
+		// x != 2 -> Resultado: V (true)
+		System.out.println("x != 2: " + (x != 2)); 
+		
+    }
+}
