0916b9c62ff7ff9a0625486c9d2870d80564c2e3
Leila Giangiardi <leila.giangiardi@ufrpe.br>
2026-03-10 21:36:49 -0300

    🐛 refactor(seção 5): corrige erro de digitação no nome do arquivo A07

---
+++ src/sc05_estrutura_condicional/A07_exercicio4.java
@@ -0,0 +1,27 @@
+package src.sc05_estrutura_condicional;
+
+import java.util.Scanner;
+
+public class A07_exercicio {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a hora inicial e a hora final do jogo (formato 0-23h): ");
+        int horaInicial = sc.nextInt();
+        int horaFinal = sc.nextInt();
+
+        int duracao;
+        
+        if (horaInicial < horaFinal) {
+            // Caso simples: começou e terminou no mesmo dia
+            duracao = horaFinal - horaInicial;
+        } else {
+            // Caso complexo: passou pela meia-noite 
+            duracao = 24 - horaInicial + horaFinal;
+        }
+
+        System.out.println("O jogo durou " + duracao + " horas(s)");
+
+        sc.close();
+    }
+}
