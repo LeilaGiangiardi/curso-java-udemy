08bb7481d31504d246835df4228377e579f3bd7e
Leila Giangiardi <leila.giangiardi@ufrpe.br>
2026-03-10 21:13:20 -0300

    ✨ feat(seção 5): exercício 01 - verificação de número negativo

---
+++ src/sc05_estrutura_condicional/A04_exercicio1.java
@@ -0,0 +1,24 @@
+package src.sc05_estrutura_condicional;
+
+import java.util.Scanner;
+
+public class A04_exercicio1 {
+    public static void main(String[] args) {
+        
+        int num;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número inteiro: ");
+        num = sc.nextInt();
+
+        if (num>0) {
+            System.out.println("O número é positivo");
+        }
+        else{
+            System.out.println("O número é negativo");
+        }
+        sc.close();
+        
+    }
+    
+}
