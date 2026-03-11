ff77738f62f3143d19bc732f7158c054408fba20
Leila Giangiardi <leila.giangiardi@ufrpe.br>
2026-03-10 21:20:19 -0300

    ✨ feat(seção 5): exercício 03 - verificação de números múltiplos

---
+++ src/sc05_estrutura_condicional/A06_exercicio3.java
@@ -0,0 +1,23 @@
+package src.sc05_estrutura_condicional;
+import java.util.Scanner;
+
+public class A06_exercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Lendo os dois valores inteiros 
+        System.out.println("Digite dois números inteiros:");
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        // || -> "OU"
+        if (A % B == 0 || B % A == 0) {
+            System.out.println("São multiplos");
+        } else {
+            System.out.println("Nao são multiplos");
+        }
+
+        sc.close();
+    }
+}
+
