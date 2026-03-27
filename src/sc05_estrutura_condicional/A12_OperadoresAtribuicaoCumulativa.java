2c9a161fa74e09000564283f3713f3965641cb25
Leila Giangiardi <leila.giangiardi@ufrpe.br>
2026-03-27 10:34:06 -0300

    ✨ feat(seção 5): implementa operadores de atribuicao cumulativa

---
+++ src/sc05_estrutura_condicional/A12_OperadoresAtribuicaoCumulativa.java
@@ -0,0 +1,29 @@
+package src.sc05_estrutura_condicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class A12_OperadoresAtribuicaoCumulativa {
+
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de minutos consumidos: ");
+        int minutos = sc.nextInt();
+
+        double conta = 50.0; // Valor da franquia básica do plano
+
+        if (minutos > 100) {
+            /* * Uso do Operador de Atribuição Cumulativa (+=)
+             * Em vez de: conta = conta + (minutos - 100) * 2.0;
+             */
+            conta += (minutos - 100) * 2.0;
+        }
+
+        System.out.printf("Valor da conta = R$ %.2f%n", conta);
+
+        sc.close();
+    }
+}
