569acba0cdc592991a6fd922e54721680ce63e4e
Leila Giangiardi <leila.giangiardi@ufrpe.br>
2026-03-31 13:45:04 -0300

    🎉 feat(seção 6): inicia seção 06 e adiciona exemplo de debug

---
+++ src/sc06_estruturas_repetitivas/A01_debug.java
@@ -0,0 +1,12 @@
+package sc06_estruturas_repetitivas;
+
+public class A01_debug {
+    public static void main(String[] args) {
+        int x = 0;
+        
+        while (x < 5) {
+            System.out.println("Valor de x: " + x);
+            x = x + 1; // Coloque o Breakpoint aqui!
+        }
+    }
+}
