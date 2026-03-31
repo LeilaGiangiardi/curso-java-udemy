107db8ebe7ecb6a8a02ca06c552476cf216db72c
Leila Giangiardi <leila.giangiardi@ufrpe.br>
2026-03-31 19:40:40 -0300

    ✨ feat(seção 6): resolve exercício de divisão com verificação de zero e casting

---
+++ src/sc06_estruturas_repetitivas/A10_exercicio7.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class A10_exercicio7 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos pares de números você vai digitar? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            // Verificação do denominador zero
+            if (y == 0) {
+                System.out.println("divisao impossivel");
+            } 
+            else {
+                /* Fazemos um "casting" (double) para que o Java entenda 
+                   que queremos o resultado com casas decimais. */
+                double divisao = (double) x / y;
+                System.out.printf("%.1f%n", divisao);
+            }
+        }
+
+        sc.close();
+    }
+}
