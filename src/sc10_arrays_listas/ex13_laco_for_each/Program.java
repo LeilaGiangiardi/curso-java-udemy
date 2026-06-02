56050ee0614cfc77cdf99357536e2971387644c0
Leila Giangiardi <leila.giangiardi@ufrpe.br>
2026-06-02 20:20:26 -0300

    ✨ feat(seção 10): Laço for each

---
+++ src/sc10_arrays_listas/ex13_laco_for_each/Program.java
@@ -0,0 +1,15 @@
+package sc10_arrays_listas.ex13_laco_for_each;
+
+public class Program {
+
+    public static void main(String[] args) {
+        String[] vect = new String[] {"Maria", "Bob", "Alex"};
+
+        /*(Tipo Apelido e nome da lista */
+        for (String cada_elemento: vect){
+            System.out.println(cada_elemento);
+
+            }  
+    }
+    
+}
