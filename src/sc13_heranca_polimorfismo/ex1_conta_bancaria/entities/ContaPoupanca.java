0d09670304fd2e25eef78f18b98c1c0ba0bac2c4
Leila Giangiardi <leila.giangiardi@ufrpe.br>
2026-08-20 20:41:12 -0300

    🔒 feat(seção13): aplica a palavra final em métodos para proteção de sobrescrita
    Adiciona comentários de estudo sobre o uso do modificador final em classes e métodos, aplicando a proteção no método saque da ContaPoupanca para evitar alterações na regra de negócio em futuras subclasses.

--- src/sc13_heranca_polimorfismo/ex1_conta_bancaria/entities/ContaPoupanca.java
+++ src/sc13_heranca_polimorfismo/ex1_conta_bancaria/entities/ContaPoupanca.java
@@ -1,6 +1,12 @@
 package sc13_heranca_polimorfismo.ex1_conta_bancaria.entities;
 
-public class ContaPoupanca extends Conta{
+
+// 1. FINAL EM CLASSES:
+// Se colocarmos 'final' na declaração da classe (public final class ContaPoupanca),
+// NENHUMA outra classe poderá herdar (extends) de ContaPoupanca.
+// Isso garante segurança (ninguém vai criar uma sub-poupança) 
+
+public final class ContaPoupanca extends Conta{
     private Double taxaDeJuros;
 
     public ContaPoupanca() {
@@ -24,8 +30,12 @@ public class ContaPoupanca extends Conta{
         saldo += saldo*taxaDeJuros;
     }
     
+    // 2. FINAL EM MÉTODOS:
+    // A palavra 'final' aqui garante que este método NÃO poderá ser sobreposto (@Override) 
+    // caso alguma classe herde de ContaPoupanca no futuro.
+    // protege regras de negócio que não podem mudar
     @Override //procura o metodo na superclasse
-    public void saque(Double quantia){
+    public final void saque(Double quantia){
         saldo -= quantia; // Sem a taxa de 5.0
     }
     
